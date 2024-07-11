341a67c879c683d9fd80f807f241cd62f17197ce
clarabecker <Claraeu15@outlook.com>
2024-07-11 15:56:32 -0300

    Changes in instance_generator.

--- src/instance_generator.py
+++ src/instance_generator.py
@@ -1,86 +1,70 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from retangulo_demanda import RetanguloDemanda
 
-def criarFigura (largura, altura, num_pontos_x, num_pontos_y, path_image, num_totaldemanda, perc_p):
+def criarFigura (largura, altura, num_pontos_x, num_pontos_y, path_image):
     fig, ax = plt.subplots()
     retangulo = plt.Rectangle((0, 0), largura, altura, edgecolor='black', facecolor='none')
     ax.add_patch(retangulo)
-    aux_demanda = []
-    demanda = []
-    p = int((num_pontos_x * num_pontos_y) * perc_p)
 
-    cap = int((num_totaldemanda/p) * 1.7)
-
-    # Adicionar uma imagem de fundo ao retângulo
     img = plt.imread(path_image)
     ax.imshow(img, extent=[0, largura, 0, altura], aspect='auto')
 
-    # Calcular as coordenadas dos pontos no grid usando distância de Manhattan
-    x_coords = np.linspace(largura*0.05, largura*0.95, num_pontos_x)  # Ajuste para não ficar nas bordas
-    y_coords = np.linspace(altura*0.05, altura*0.95, num_pontos_y)    # Ajuste para não ficar nas bordas
+    # distância de Manhattan
+    x_coords = np.linspace(largura*0.05, largura*0.95, num_pontos_x)
+    y_coords = np.linspace(altura*0.05, altura*0.95, num_pontos_y)
 
-    # Gerar todas as combinações de coordenadas
     pontos_grid = np.array(np.meshgrid(x_coords, y_coords)).T.reshape(-1, 2)
 
     pontos_grid = np.round(pontos_grid).astype(int)
 
-    # Gerar todas as combinações de coordenadas
     distancias_manhattan = np.zeros((len(pontos_grid), len(pontos_grid)))
 
     for i, ponto_a in enumerate(pontos_grid):
         for j, ponto_b in enumerate(pontos_grid):
             distancias_manhattan[i, j] = abs(ponto_a[0] - ponto_b[0]) + abs(ponto_a[1] - ponto_b[1])
 
-    # Plotar os pontos do grid na figura
     ax.plot(pontos_grid[:, 0], pontos_grid[:, 1], 'bo', markersize=5)
 
-    # Adicionar rótulos aos eixos
     ax.set_xlabel('X (metros)')
     ax.set_ylabel('Y (metros)')
 
-    # Tamanho do retângulo menor
-    largura_altademanda1 = 40
-    altura_altademanda1 = 70
+    def criarFiguraRetanguloDemanda(RetanguloDemanda_obj, cor):
+        figura = plt.Rectangle(
+            (RetanguloDemanda_obj.x, RetanguloDemanda_obj.y),
+            RetanguloDemanda_obj.largura,
+            RetanguloDemanda_obj.altura,
+            edgecolor=cor,
+            facecolor='none'
+        )
+        return figura
 
-    # Calcular as coordenadas do retângulo menor
-    x_altademanda1 = 8
-    y_altademanda1 = 115
+    alta_demanda1 = RetanguloDemanda(8, 115, 70, 40)
+    figura1 = criarFiguraRetanguloDemanda(alta_demanda1, "red")
+    ax.add_patch(figura1)
 
-    # Desenhar o retângulo menor
-    retangulo_altademanda1 = plt.Rectangle((x_altademanda1, y_altademanda1), largura_altademanda1, altura_altademanda1, edgecolor='red', facecolor='none')
-    ax.add_patch(retangulo_altademanda1)
+    alta_demanda2 = RetanguloDemanda(1,80,30,90)
+    figura2 = criarFiguraRetanguloDemanda(alta_demanda2, "red")
+    ax.add_patch(figura2)
 
-    largura_altademanda2 = 90
-    altura_altademanda2 = 30
+    media_demanda = RetanguloDemanda(100,43,85,90)
+    figura3 = criarFiguraRetanguloDemanda(media_demanda, "yellow")
+    ax.add_patch(figura3)
 
-    x_altademanda2 = 1
-    y_altademanda2 = 80
-
-    # Desenhar o retângulo menor
-    retangulo_altademanda2 = plt.Rectangle((x_altademanda2, y_altademanda2), largura_altademanda2, altura_altademanda2, edgecolor='red', facecolor='none')
-    ax.add_patch(retangulo_altademanda2)
-
-    largura_mediademanda = 85
-    altura_mediademanda = 90
-
-    x_mediademanda = 100
-    y_mediademanda = 43
-
-    retangulo_mediademanda = plt.Rectangle((x_mediademanda, y_mediademanda), largura_mediademanda, altura_mediademanda, edgecolor='yellow', facecolor='none')
-    ax.add_patch(retangulo_mediademanda)
-
-    # Salvar a figura com o retângulo menor
     plt.savefig('instance_image.png')
 
-    # Mostrar a figura
     plt.show()
 
+    return pontos_grid, alta_demanda1, alta_demanda2, media_demanda, distancias_manhattan
+
+def calcular_demanda(pontos_grid, alta_demanda1, alta_demanda2, media_demanda, num_totaldemanda):
+    aux_demanda = []
     for ponto in pontos_grid:
-        if (ponto[0] >= x_altademanda1 and ponto[0] <= (x_altademanda1 + largura_altademanda1)) and (ponto[1] >= y_altademanda1 and ponto[1] <= (y_altademanda1 + altura_altademanda1)):
+        if (alta_demanda1.x <= ponto[0] <= (alta_demanda1.x + alta_demanda1.largura)) and (alta_demanda1.y <= ponto[1] <= (alta_demanda1.y + alta_demanda1.altura)):
             aux_demanda.append(1)
-        elif (ponto[0] >= x_altademanda2 and ponto[0] <= (x_altademanda2 + largura_altademanda2)) and (ponto[1] >= y_altademanda2  and ponto[1] <= (y_altademanda2 + altura_altademanda2)):
+        elif (alta_demanda2.x <= ponto[0] <= (alta_demanda2.x + alta_demanda2.largura)) and (alta_demanda1.y <= ponto[1] <= (alta_demanda1.y + alta_demanda1.altura)):
             aux_demanda.append(1)
-        elif (ponto[0] >= x_mediademanda and ponto[0] <= (x_mediademanda + largura_mediademanda)) and (ponto[1] >= y_mediademanda and ponto[1] <= (y_mediademanda + altura_mediademanda)):
+        elif (media_demanda.x <= ponto[0] <= (media_demanda.x + media_demanda.largura)) and (media_demanda.y <= ponto[1] <= (media_demanda.y + media_demanda.altura)):
             aux_demanda.append(2)
         else:
             aux_demanda.append(3)
@@ -103,6 +87,7 @@ def criarFigura (largura, altura, num_pontos_x, num_pontos_y, path_image, num_to
 
     sobra_valores = (num_totaldemanda - ((val_altademanda * contaltademanda) + (val_mediademanda * contmediademanda) + (val_baixademanda * contbaixademanda)))
 
+    demanda = []
     for i in aux_demanda:
         if (sobra_valores > 0):
             if i == 1:
@@ -122,17 +107,26 @@ def criarFigura (largura, altura, num_pontos_x, num_pontos_y, path_image, num_to
             else:
                 demanda.append(val_baixademanda)
 
-    with open('AAD_PMEDcap_'+ str(num_pontos_x * num_pontos_y) + '_' + str(p) +'.txt', 'w') as f:
-        f.write(str(num_pontos_x * num_pontos_y) + "\n")
-        f.write(str(p) + "\n")
-        f.write(str(cap) + "\n")
-        for i in demanda:
-            f.write(str(i) + " ")
-        f.write("\n")
-        for linha in range(len(distancias_manhattan)):
-            for coluna in range(len(distancias_manhattan[linha])):
-                f.write(str(linha) + " " + str(coluna) + " " + str(int(distancias_manhattan[linha, coluna])) + "\n")
+    return demanda
+def salvar_dados(num_pontos_x, num_pontos_y, p, cap, demanda, distancias_manhattan):
+    with open(f'AAD_PMEDcap_{num_pontos_x * num_pontos_y}_{p}.txt', 'w') as f:
+        f.write(f"{num_pontos_x * num_pontos_y}\n{p}\n{cap}\n")
+        f.write(" ".join(map(str, demanda)) + "\n")
+        for linha, colunas in enumerate(distancias_manhattan):
+            for coluna, distancia in enumerate(colunas):
+                f.write(f"{linha} {coluna} {int(distancia)}\n")
 
 
 if __name__ == '__main__':
-    criarFigura(230, 190, 10, 10, 'C:\\P-Medianas-Capacitado\\image_map.jpeg', 500, 0.3)
+    perc_p = 0.3
+    num_totaldemanda = 500
+    num_pontos_x = 5
+    num_pontos_y = 5
+
+    p = int((num_pontos_x * num_pontos_y) * perc_p)
+    cap = int((num_totaldemanda/p) * 1.7)
+
+    pontos_grid, alta_demanda1, alta_demanda2, media_demanda, distancias_manhattan = criarFigura(230, 190, num_pontos_x, num_pontos_y, 'C:\\P-Medianas-Capacitado\\image_map.jpeg')
+    demanda = calcular_demanda(pontos_grid, alta_demanda1, alta_demanda2, media_demanda, num_totaldemanda)
+    salvar_dados(num_pontos_x, num_pontos_y, p, cap, demanda, distancias_manhattan)
+
---
+++ src/retangulo_demanda.py
@@ -0,0 +1,6 @@
+class RetanguloDemanda:
+    def __init__(self, x, y, altura, largura) -> None:
+        self.x = x
+        self.y = y
+        self.altura = altura
+        self.largura = largura
